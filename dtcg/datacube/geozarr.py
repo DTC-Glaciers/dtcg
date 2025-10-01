10eb00ae2b29f94d628f38eb4dcc9f1f25171f3a
carolynmichael19 <carolyn.michael@earthwave.co.uk>
2025-10-01 18:04:43 +0100

    data tree helper

--- dtcg/datacube/geozarr.py
+++ dtcg/datacube/geozarr.py
@@ -34,6 +34,7 @@ class GeoZarrHandler(MetadataMapper):
     def __init__(
         self: GeoZarrHandler,
         ds: xr.Dataset,
+        ds_name: str = "L1",
         target_chunk_mb: float = 5.0,
         compressor: Optional[Blosc] = None,
         metadata_mapping_file_path: str = None,
@@ -46,6 +47,8 @@ class GeoZarrHandler(MetadataMapper):
         ds : xarray.Dataset
             Input dataset with dimensions ('x', 'y') or ('t', 'x', 'y').
             Must include coordinate variables.
+        ds_name : str, default 'L1'
+            Name of datacube.
         target_chunk_mb : float, default 5.0
             Approximate chunk size in megabytes for efficient storage.
         compressor : Blosc, default None
@@ -59,21 +62,29 @@ class GeoZarrHandler(MetadataMapper):
             Zarr format version to use (2 or 3).
         """
         super().__init__(metadata_mapping_file_path=metadata_mapping_file_path)
-        self.ds = ds.copy(deep=True)
         self.target_chunk_mb = target_chunk_mb
         self.compressor = compressor or Blosc(
             cname="zstd", clevel=3, shuffle=Blosc.BITSHUFFLE
         )
         self.zarr_format = zarr_format
+
+        ds = self._validate_dataset(ds)
+        ds = self._update_metadata(ds)
         self.encoding = {}
+        self._define_encodings(ds, ds_name)
 
-        self._validate_dataset()
-        self._define_encodings()
-        self._update_metadata()
+        # convert dataset to datatree
+        self.ds = xr.DataTree.from_dict({ds_name: ds})
 
-    def _validate_dataset(self: GeoZarrHandler) -> None:
+    def _validate_dataset(self: GeoZarrHandler, ds: xr.Dataset) -> xr.Dataset:
         """Validate the input dataset to ensure it includes required
         dimensions and associated coordinate variables.
+
+        Parameters
+        ----------
+        ds : xarray.Dataset
+            Input dataset with dimensions ('x', 'y') or ('t', 'x', 'y').
+            Must include coordinate variables.
 
         Raises
         ------
@@ -83,17 +94,18 @@ class GeoZarrHandler(MetadataMapper):
               variable.
         """
         accepted_dims = {"x", "y", "t"}
-        if not set(self.ds.dims).issubset(accepted_dims):
+        if not set(ds.dims).issubset(accepted_dims):
             raise ValueError(
                 "Incorrect dataset dimensions."
                 f" Accepted data dimensions are: {accepted_dims}"
             )
-        for dim in self.ds.dims:
-            if dim not in self.ds.coords:
+        for dim in ds.dims:
+            if dim not in ds.coords:
                 raise ValueError(
                     f"Coordinate variable for dimension '{dim}' is missing in "
                     "the dataset."
                 )
+        return ds
 
     def _calculate_chunk_sizes(
         self: GeoZarrHandler, var: xr.DataArray
@@ -140,32 +152,47 @@ class GeoZarrHandler(MetadataMapper):
 
         return chunk_sizes
 
-    def _define_encodings(self: GeoZarrHandler) -> None:
+    def _define_encodings(self: GeoZarrHandler, ds: xr.Dataset, ds_name: str) -> None:
         """Define encoding settings for each data variable in the
         dataset, including chunking and compression.
+
+        Parameters
+        ----------
+        ds : xarray.Dataset
+            Input dataset with dimensions ('x', 'y') or ('t', 'x', 'y').
+            Must include coordinate variables.
+        ds_name : str
+            Dataset name to be used for this node of the tree.
 
         Notes
         -----
         Chunk sizes are computed using `_calculate_chunk_sizes`, and the
         compressor is set according to the class-level setting.
         """
-        for var in self.ds.data_vars:
-            chunk_sizes = self._calculate_chunk_sizes(self.ds[var])
-            chunks = tuple(chunk_sizes.get(dim) for dim in self.ds[var].dims)
-            self.encoding[var] = {"chunks": chunks, "compressor": self.compressor}
+        for var in ds.data_vars:
+            chunk_sizes = self._calculate_chunk_sizes(ds[var])
+            chunks = tuple(chunk_sizes.get(dim) for dim in ds[var].dims)
+            self.encoding[f"{ds_name}/{var}"] = {"chunks": chunks, "compressor": self.compressor}
 
-    def _update_metadata(self) -> None:
+    def _update_metadata(self, ds: xr.Dataset) -> xr.Dataset:
         """Update metadata to Climate and Forecast convention.
+
+        Parameters
+        ----------
+        ds : xarray.Dataset
+            Input dataset with dimensions ('x', 'y') or ('t', 'x', 'y').
+            Must include coordinate variables.
 
         Metadata is first updated using the ``update_metadata`` method.
         Each data variable is tagged with the ``grid_mapping`` attribute
         for spatial referencing.
         """
-        self.ds = self.update_metadata(self.ds)
-        for var in self.ds.data_vars:
-            var_dims = self.ds[var].dims
+        ds = self.update_metadata(ds)
+        for var in ds.data_vars:
+            var_dims = ds[var].dims
             if "x" in var_dims or "y" in var_dims:
-                self.ds[var].attrs["grid_mapping"] = "spatial_ref"
+                ds[var].attrs["grid_mapping"] = "spatial_ref"
+        return ds
 
     def export(
         self: GeoZarrHandler, storage_directory: str, overwrite: bool = True
@@ -193,12 +220,27 @@ class GeoZarrHandler(MetadataMapper):
             encoding=self.encoding,
         )
 
-    def add_layer(self: GeoZarrHandler, new_layer: xr.Dataset):
-        # some validation of dataset attributes
-        self.METADATA_SCHEMA.validate(new_layer.attrs)
+    def add_layer(self: GeoZarrHandler, ds: xr.Dataset, ds_name: str) -> None:
+        """Add a new dataset as a child group of the DataTree at the root.
 
-        # # some functionality for adding the new layer to the existing
-        # # dataset
-        # # either a new var, or data tree, needs to handle different
-        # # dimensionality
-        # self.ds[new_layer] = new_layer
+        Parameters
+        ----------
+        ds : xarray.Dataset
+            New dataset layer to be added to the existing data tree.
+        ds_name : str
+            Layer name to be used for this node of the tree.
+        """
+        if ds_name in self.ds.children:
+            raise ValueError(f"Group '{ds_name}' already exists.")
+
+        # prepare new dataset
+        ds = self._validate_dataset(ds)
+        ds = self._update_metadata(ds)
+
+        # append additional encodings to the encodings class attribute
+        self._define_encodings(ds, ds_name)
+
+        # validate dataset attributes
+        self.METADATA_SCHEMA.validate(ds.attrs)
+
+        self.ds[ds_name] = xr.DataTree(dataset=ds)
