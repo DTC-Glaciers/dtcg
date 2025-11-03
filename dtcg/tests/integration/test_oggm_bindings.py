4b4a18f03cc039ef0b381546f749e554a5bff161
gampnico <45390064+gampnico@users.noreply.github.com>
2025-11-03 11:12:46 +0000

    fix(tests): key lookup

--- dtcg/tests/integration/test_oggm_bindings.py
+++ dtcg/tests/integration/test_oggm_bindings.py
@@ -208,7 +208,7 @@ class TestOGGMBindings:
         for key in keys:
             assert key in details.keys()
             assert isinstance(details[key], dict)
-            assert isinstance(details["value"], str)
-            assert isinstance(details["unit"], str)
+            assert isinstance(details[key]["value"], str)
+            assert isinstance(details[key]["unit"], str)
 
         assert details["Name"]["value"] == "Hintereisferner"
