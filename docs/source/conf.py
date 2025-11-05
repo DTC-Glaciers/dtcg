bb66bc810ea30a9b6fe4da93b0f5d5a90cd81bf8
gampnico <45390064+gampnico@users.noreply.github.com>
2025-11-05 15:22:41 +0000

    feat: dynamic documentation update
    Run ``make html`` from the ``docs/`` directory to automatically generate
    an API reference.

---
+++ docs/source/conf.py
@@ -0,0 +1,87 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# pylint: skip-file
+# flake8: noqa: F541
+
+import os
+import sys
+from datetime import date
+
+sys.path.insert(0, os.path.abspath("../.."))
+
+# -- Remove Copyright Boilerplate --------------------------------------------
+
+def remove_boilerplate(app, what, name, obj, options, lines):
+    if what == "module":
+        del lines[:16]
+
+
+def setup(app):
+    app.connect("autodoc-process-docstring", remove_boilerplate)
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "DTCG"
+copyright = f"{date.today().year}, DTCG Contributors"
+author = "DTCG Contributors"
+release = "0.4.3"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.todo",
+    # "shibuya",
+    "sphinx.ext.githubpages",
+    "sphinx.ext.viewcode",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+# -- Autodoc configuration ---------------------------------------------------
+
+# autodoc_member_order = 'bysource'
+autodoc_default_flags = [
+    "members",
+    "undoc-members",
+    "show-inheritance",
+]
+autodoc_typehints = "description"
+autodoc_type_aliases = {
+    "GlacierDirectory": "oggm.GlacierDirectory",
+    "GeoZarrHandler": "dtcg.datacube.geozarr.GeoZarrHandler",
+}
+autosummary_generate = True
+
+numpydoc_class_members_toctree = True
+numpydoc_show_class_members = False
+
+napoleon_use_param = True
+napoleon_use_rtype = True
+
+pygments_style = "sphinx"
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinx_book_theme"
+html_static_path = ["_static"]
+
+master_doc = "index"
+trim_footnote_reference_space = True
+html_show_sphinx = False
+html_static_path = ["_static"]
+highlight_language = "python"
+html_title = "DTCG"
+html_logo = "./_static/dtcg_logo_transparent.png"
+html_favicon = "./_static/favicon.ico"
---
+++ docs/source/index.rst
@@ -0,0 +1,21 @@
+.. dtcg documentation master file, created by
+   sphinx-quickstart on Mon Nov  3 11:46:49 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Introduction
+============
+
+DTCG introduction.
+
+.. toctree::
+   :maxdepth: 3
+   :caption: Installation
+
+   installation
+
+.. toctree::
+   :maxdepth: 3
+   :caption: API Reference
+
+   api
