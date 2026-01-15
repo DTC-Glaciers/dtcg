e42f60640025dc159f31238638f40b41752f1c20
gampnico <45390064+gampnico@users.noreply.github.com>
2026-01-15 12:44:59 +0000

    build: bump version number

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,7 @@ def setup(app):
 project = "DTCG"
 copyright = f"{date.today().year}, DTCG Contributors"
 author = "DTCG Contributors"
-release = "0.6.5"
+release = "0.7.0"
 version = os.environ.get("READTHEDOCS_VERSION", "latest")
 
 # -- General configuration ---------------------------------------------------
